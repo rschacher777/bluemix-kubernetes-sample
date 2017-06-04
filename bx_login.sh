faecf3b9044b92222a526e28cc4eba7f9336a10b
Ritchie Schacher <schacher@us.ibm.com>
2017-06-04 12:53:21 +0000

    use API Key

--- bx_login.sh
+++ bx_login.sh
@@ -15,7 +15,8 @@ fi
 echo "Deploy pods"
 
 echo "bx login -a $CF_TARGET_URL"
-bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+# bx login -a "$CF_TARGET_URL" -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
+bx login -a "$CF_TARGET_URL" --apikey "$BLUEMIX_PASSWORD" -c "$BLUEMIX_ACCOUNT" -o "$CF_ORG" -s "$CF_SPACE"
 if [ $? -ne 0 ]; then
   echo "Failed to authenticate to Bluemix"
   exit 1
